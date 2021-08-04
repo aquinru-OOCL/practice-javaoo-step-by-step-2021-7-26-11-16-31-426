37462503af900ad5e39554742dbafd1363e4cb10
AQUINRU <russel.aquino@oocl.com>
2021-08-04 19:53:36 +0800

    [Practice06Test][Russel]feat: implemented Teacher class and modified Person & Student class

--- src/main/java/practice06/Klass.java
+++ src/main/java/practice06/Klass.java
@@ -1,4 +1,18 @@
 package practice06;
 
 public class Klass {
+    private final int klass;
+
+    public Klass(int klass) {
+        this.klass = klass;
+    }
+
+
+    public int getNumber() {
+        return klass;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %s", klass);
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,39 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+    String teacherString = "I am a Teacher.";
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        String classString = "I teach No Class.";
+
+        if (klass != null) {
+            classString = String.format("I teach Class %s.", klass.getNumber());
+        }
+
+        return String.format("%s %s %s", super.introduce(), teacherString, classString);
+    }
+
+    public String introduceWith(Student student) {
+        String classString = "I don't teach";
+
+        if (student.getKlass().getNumber() == klass.getNumber()) {
+            classString = "I teach";
+        }
+        return String.format("%s %s %s %s.", super.introduce(), teacherString, classString, student.getName());
+    }
+
 }
