9783ebb5d1a275c753987f78e4794ef646e378ff
AQUINRU <russel.aquino@oocl.com>
2021-08-04 17:30:21 +0800

    [Practice05Test][Russel]feat: implemented Teacher class

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,28 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends  Person{
+    private int klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        String classString = "I teach Class " + klass + ".";
+
+        if (klass == 0) {
+            classString = "I teach No Class.";
+        }
+
+        return super.introduce() + " I am a Teacher. " + classString;
+    }
 }
